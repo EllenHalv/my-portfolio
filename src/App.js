9dd3e387e2fb7c0db820668d3c7e167396cee45d
ellen <ellenhalvardsson@gmail.com>
2025-01-07 18:11:02 +0100

    added purple box shadow to cards

--- src/App.js
+++ src/App.js
@@ -141,9 +141,9 @@ const Projects = () => (
 
 const Contact = () => (
     <section>
-        <h2>Contact <FontAwesomeIcon icon={faEnvelope}/></h2>
+        <h2>Contact</h2>
+        <br/>
         <div>
-
             <p>Connect with me!</p>
             <p>E-mail: <a href="mailto:ellenhalvardsson@gmail.com">ellenhalvardsson@gmail.com</a></p>
             <p>LinkedIn: <a href="https://www.linkedin.com/in/ellen-halv/" target="_blank" rel="noopener noreferrer">Ellen
@@ -155,15 +155,17 @@ const Contact = () => (
 const About = () => (
     <section>
         <h2>About</h2>
+        <br/>
         <div>
             <p>Hi, I'm Ellen Halvardsson, a software developer with a passion for cloud technologies and full-stack
                 development.
                 I thrive on creativity and problem-solving, combining technical skills with innovative thinking to build
                 solutions that improve everyday life.
-                Beyond coding, I enjoy crafting and exploring ways to express myself through design and technology. I enjoy knitting, crocheting and digital design,
+                Beyond coding, I enjoy crafting and exploring ways to express myself through design and technology. I
+                enjoy knitting, crocheting and digital design,
                 and I'm always seeking new inspiration and opportunities for growth.</p>
         </div>
-        <br/>
+        <br/><br/>
         <div>
             <p>Download my CV <FontAwesomeIcon icon={faArrowRight}/>
                 {' '}

6e86285a4ce7a555044904302805a9d2a2583bb5
ellen <ellenhalvardsson@gmail.com>
2025-01-10 10:53:08 +0100

    added md navbar menu toggler

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+// import React from 'react';
+import { React, useState } from 'react';
 import { Link } from "react-router-dom";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEnvelope, faProjectDiagram, faKey, faDatabase, faMessage, faArrowRight, faInfinity, faNetworkWired, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import styles from './../../App.css';
+import './Navbar.module.css'; // Import custom CSS for styling
 
-const Navbar = () => {
+/*const Navbar = () => {
+    const [nav, setNav] = useState(false);
+
     return (
         <header className="main-header">
             <nav className="navbar navbar-expand-md navbar-dark bg-darkgrey">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">Home</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                        <span className="navbar-toggler-icon"></span>
+                    <button onClick={() => setNav(!nav)}
+                            className="navbar-toggler"
+                            type="button"
+                    >
+                        {nav ? <FontAwesomeIcon icon={faBars}/> : <FontAwesomeIcon icon={faXmark}/>}
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
+                        <ul className={nav ? [styles.navbarNav, styles.active].join(' ') : [styles.navbarNav]}>
                             <li className="nav-item">
                                 <Link className="nav-link" to="/projects">Projects</Link>
                             </li>
@@ -24,6 +34,59 @@ const Navbar = () => {
                         </ul>
                     </div>
                 </div>
+            </nav>
+        </header>
+    );
+};*/
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false);
+
+    const toggleNav = () => {
+        setNav(!nav);
+    };
+
+    const closeNav = () => {
+        setNav(false);
+    };
+
+    return (
+        <header className="main-header">
+            <nav className="navbar navbar-expand-md navbar-dark bg-darkgrey">
+                {/*<div className="container-fluid">*/}
+                    {/* Home Link */}
+                    <Link className="navbar-brand" to="/">Home</Link>
+
+                    {/* Toggler Button */}
+                    <button
+                        onClick={toggleNav}
+                        className="navbar-toggler"
+                        type="button"
+                    >
+                        <FontAwesomeIcon icon={nav ? faXmark : faBars} />
+                    </button>
+
+                    {/* Sliding Menu */}
+                    <div className={nav ? "navbar-nav active" : "navbar-nav"}>
+                        <ul>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/projects" onClick={closeNav}>
+                                    Projects
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/about" onClick={closeNav}>
+                                    About
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact" onClick={closeNav}>
+                                    Contact
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                {/*</div>*/}
             </nav>
         </header>
     );
